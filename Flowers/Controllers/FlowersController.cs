3136d8958abc4e946c9f0806adbd9470a8a5787e
BasharIraqi <bashar.oroq@hotmail.com>
2022-05-12 22:19:58 +0300

    Add project files.

---
+++ Flowers/Controllers/FlowersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Flowers.Controllers
+{
+    [ApiController]
+    [Route("flowers/[action]")]
+    public class FlowersController :ControllerBase
+    {
+        [HttpGet]
+        public string Aster()
+        {
+            return "Aster";
+        }
+        [HttpGet]
+        public string Azalea()
+        {
+            return "Azalea";
+        }
+        [HttpGet]
+        public string Buttercup()
+        {
+            return "Buttercup";
+        }
+        [HttpGet]
+        public string Crocus()
+        {
+            return "Crocus";
+        }
+    }
+    
+}
